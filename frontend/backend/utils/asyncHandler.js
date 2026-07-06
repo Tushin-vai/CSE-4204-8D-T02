11abe1498c7c9724a45129caa4c67e144039c835
juena-tabassum-tae <147265748+juena-tabassum-tae@users.noreply.github.com>
2026-07-06 14:08:04 +0600

    Delete frontend/backend directory

--- frontend/backend/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// utils/asyncHandler.js
-// Wraps async controllers so any thrown error is passed to Express error handler
-// This means we don't need try/catch in every controller function
-const asyncHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
-
-module.exports = asyncHandler;
